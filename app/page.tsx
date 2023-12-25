18c70cfd0573f1bd398ed8e88804a1e82555f47c
Igor Papandinas <igor.papandinas@posteo.net>
2023-12-25 23:28:13 +0100

    useless files deleted

--- app/page.tsx
+++ app/page.tsx
@@ -24,13 +24,6 @@ export default function Home() {
 			<div className="flex gap-3">
 				<Link
 					isExternal
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
 					className={buttonStyles({ variant: "bordered", radius: "full" })}
 					href={siteConfig.links.github}
 				>
