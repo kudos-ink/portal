b58d4ea9bb338217c39d526a81a4fb91a8a8df40
Leandro <leapalazzolo@gmail.com>
2023-12-27 12:29:39 -0300

    feat: add skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,40 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import Search from "../components/search";
 
 export default function Home() {
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
-			</div>
-
-			<div className="flex gap-3">
-				<Link
-					isExternal
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
-
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
-			</div>
-		</section>
-	);
+  return (
+    <div>
+      <section className="flex flex-col items-center py-8 md:py-10">
+        <h1 className={title()}>Find Collaborations,</h1>
+        <h1 className={title()}>Collect Kudos</h1>
+      </section>
+      <div className="flex flex-col items-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <Search />
+          <h2 className={subtitle({ class: "mt-4" })}>Banner</h2>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="flex justify-start gap-4">
+          <div>
+            <h2 className={subtitle({ class: "mt-4" })}>Filter 1</h2>
+          </div>
+          <div>
+            <h2 className={subtitle({ class: "mt-4" })}>Filter 2</h2>
+          </div>
+          <div>
+            <h2 className={subtitle({ class: "mt-4" })}>Filter 3</h2>
+          </div>
+          <div>
+            <h2 className={subtitle({ class: "mt-4" })}>Clear</h2>
+          </div>
+        </div>
+        <div className="flex justify-end">
+          <div>
+            <h2 className={subtitle({ class: "mt-4" })}>Sort</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
