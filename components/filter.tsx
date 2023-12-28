ce53de4e6027d19538f2538115c1bf893a713151
Leandro <leapalazzolo@gmail.com>
2023-12-28 12:04:13 -0300

    feat: add filter component

---
+++ components/filter.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { FC } from "react";
+import { Select, SelectItem } from "@nextui-org/react";
+import Emoji from "./utils/emoji";
+import { Item } from "../components/utils/types/item";
+
+interface FilterProps {
+  placeholder: string;
+  emoji: string;
+  items: Item[];
+}
+export const Filter: FC<FilterProps> = ({ placeholder, items, emoji }) => {
+  return (
+    <Select
+      placeholder={placeholder}
+      startContent={<Emoji emoji={emoji} className="text-sm" />}
+      size={"sm"}
+    >
+      {items.map((item) => (
+        <SelectItem
+          key={item.value}
+          value={item.value}
+          startContent={<Emoji emoji={item.emoji} />}
+        >
+          {item.label}
+        </SelectItem>
+      ))}
+    </Select>
+  );
+};
+
+export default Filter;
