1306919ab62b235b88157219ef2ff25a86ec37bc
Leandro <leapalazzolo@gmail.com>
2023-12-28 12:05:17 -0300

    chore: add data for filters and autocomplete

---
+++ data/items.tsx
@@ -0,0 +1,109 @@
+export const search = [
+  { value: "senior", emoji: "👵", label: "Senior" },
+  { value: "engineer", emoji: "🤓", label: "Engineer" },
+  { value: "dev", emoji: "🤓", label: "Developer" },
+  { value: "finance", emoji: "💰", label: "Finance" },
+  { value: "sys-admin", emoji: "♾️", label: "Sys Admin" },
+  { value: "javascript", emoji: "☕️", label: "JavaScript" },
+  // { value: "exec", emoji: "💼", label: "Executive" },
+  { value: "backend", emoji: "🍑", label: "Backend" },
+  { value: "golang", emoji: "🐀", label: "Golang" },
+  { value: "cloud", emoji: "☁️", label: "Cloud" },
+  // { value: "medical", emoji: "🚑", label: "Medical" },
+  { value: "front-end", emoji: "🎨", label: "Front End" },
+  { value: "full-stack", emoji: "🥞", label: "Full Stack" },
+  { value: "ops", emoji: "♾️", label: "Ops" },
+  { value: "design", emoji: "🎨", label: "Design" },
+  { value: "react", emoji: "⚛️", label: "React" },
+  { value: "infosec", emoji: "🔑", label: "InfoSec" },
+  { value: "marketing", emoji: "🚥", label: "Marketing" },
+  { value: "mobile", emoji: "📱", label: "Mobile" },
+  { value: "content-writing", emoji: "✍️", label: "Content Writing" },
+  { value: "saas", emoji: "📦", label: "SaaS" },
+  { value: "recruiter", emoji: "🎯", label: "Recruiter" },
+  { value: "full-time", emoji: "⏰", label: "Full Time" },
+  { value: "api", emoji: "🤖", label: "API" },
+  // { value: "sales", emoji: "💼", label: "Sales" },
+  { value: "ruby", emoji: "💎", label: "Ruby" },
+  { value: "education", emoji: "👨‍🏫", label: "Education" },
+  { value: "devops", emoji: "♾️", label: "DevOps" },
+  { value: "stats", emoji: "👩‍🔬", label: "Stats" },
+  { value: "python", emoji: "🐍", label: "Python" },
+  { value: "node", emoji: "🔗", label: "Node" },
+  // { value: "english", emoji: "🇬🇧", label: "English" },
+  { value: "non-tech", emoji: "🔌", label: "Non Tech" },
+  { value: "video", emoji: "📼", label: "Video" },
+  // { value: "travel", emoji: "🎒", label: "Travel" },
+  { value: "quality-assurance", emoji: "🔬", label: "Quality Assurance" },
+  // { value: "ecommerce", emoji: "🛍", label: "Ecommerce" },
+  { value: "teaching", emoji: "👨‍🏫", label: "Teaching" },
+  { value: "linux", emoji: "🐧", label: "Linux" },
+  { value: "java", emoji: "☕️", label: "Java" },
+  { value: "crypto", emoji: "🏅", label: "Crypto" },
+  { value: "junior", emoji: "👶", label: "Junior" },
+  { value: "git", emoji: "📦", label: "Git" },
+  { value: "legal", emoji: "👩‍⚖️", label: "Legal" },
+  { value: "android", emoji: "🤖", label: "Android" },
+  // { value: "accounting", emoji: "💼", label: "Accounting" },
+  // { value: "admin", emoji: "♾️", label: "Admin" },
+  // { value: "microsoft", emoji: "🖼", label: "Microsoft" },
+  // { value: "excel", emoji: "📗", label: "Excel" },
+  { value: "php", emoji: "🐘", label: "PHP" },
+];
+
+export const languages = [
+  { value: "Assembly", label: "Assembly", emoji: "🔧" }, // Assembly
+  //   { value: "Astro", label: "Astro", emoji: "" },
+  //   { value: "Batchfile", label: "Batchfile", emoji: "" },
+  { value: "C", label: "C", emoji: "🐉" }, // C
+  { value: "C#", label: "C#", emoji: "♯" }, // C#
+  { value: "C++", label: "C++", emoji: "🚀" }, // C++
+  //   { value: "CMake", label: "CMake", emoji: "" },
+  { value: "CSS", label: "CSS", emoji: "🎨" }, // CSS
+  { value: "Cairo", label: "Cairo", emoji: "🖌️" }, // Cairo
+  //   { value: "Circom", label: "Circom", emoji: "" },
+  //   { value: "Cython", label: "Cython", emoji: "" },
+  { value: "Dart", label: "Dart", emoji: "🎯" }, // Dart
+  { value: "Dockerfile", label: "Dockerfile", emoji: "🐳" }, // Dockerfile
+  //   { value: "EJS", label: "EJS", emoji: "" },
+  //   { value: "GLSL", label: "GLSL", emoji: "" },
+  { value: "Go", label: "Go", emoji: "🏃‍♂️" }, // Go
+  //   { value: "Groovy", label: "Groovy", emoji: "" },
+  //   { value: "HCL", label: "HCL", emoji: "" },
+  { value: "HTML", label: "HTML", emoji: "🌐" }, // HTML
+  //   { value: "Handlebars", label: "Handlebars", emoji: "" },
+  { value: "Java", label: "Java", emoji: "☕" }, // Java
+  { value: "JavaScript", label: "JavaScript", emoji: "🚀" }, // JavaScript
+  { value: "Jinja", label: "Jinja", emoji: "🌀" }, // Jinja
+  { value: "Jupyter Notebook", label: "Jupyter Notebook", emoji: "📓" }, // Jupyter Notebook
+  //   { value: "Just", label: "Just", emoji: "" },
+  { value: "Kotlin", label: "Kotlin", emoji: "🎨" }, // Kotlin
+  //   { value: "M4", label: "M4", emoji: "" },
+  //   { value: "MDX", label: "MDX", emoji: "" },
+  { value: "Makefile", label: "Makefile", emoji: "🔨" }, // Makefile
+  { value: "Move", label: "Move", emoji: "🚚" }, // Move
+  //   { value: "NSIS", label: "NSIS", emoji: "" },
+  //   { value: "Nix", label: "Nix", emoji: "" },
+  { value: "Objective-C", label: "Objective-C", emoji: "🍏" }, // Objective-C
+  { value: "PLSQL", label: "PLSQL", emoji: "🐘" }, // PLSQL
+  // { value: "PLpgSQL", label: "PLpgSQL", emoji: "🐘" }, // PLpgSQL
+  { value: "PowerShell", label: "PowerShell", emoji: "💻" }, // PowerShell
+  //   { value: "Procfile", label: "Procfile", emoji: "" },
+  //   { value: "PureBasic", label: "PureBasic", emoji: "" },
+  { value: "Python", label: "Python", emoji: "🐍" }, // Python
+  //   { value: "Roff", label: "Roff", emoji: "" },
+  { value: "Ruby", label: "Ruby", emoji: "💎" }, // Ruby
+  { value: "Rust", label: "Rust", emoji: "🦀" }, // Rust
+  { value: "SCSS", label: "SCSS", emoji: "🎨" }, // SCSS
+  //   { value: "Sage", label: "Sage", emoji: "" },
+  { value: "Shell", label: "Shell", emoji: "🐚" }, // Shell
+  //   { value: "Smarty", label: "Smarty", emoji: "" },
+  { value: "Solidity", label: "Solidity", emoji: "💸" }, // Solidity
+  //   { value: "Svelte", label: "Svelte", emoji: "" },
+  { value: "Swift", label: "Swift", emoji: "🚀" }, // Swift
+  //   { value: "TeX", label: "TeX", emoji: "" },
+  { value: "TypeScript", label: "TypeScript", emoji: "📜" }, // TypeScript
+  { value: "Vue", label: "Vue", emoji: "🖖" }, // Vue
+  //   { value: "Zig", label: "Zig", emoji: "" },
+  //   { value: "jq", label: "jq", emoji: "" },
+];
