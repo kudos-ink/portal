3072511dc0f7c5f4136dc5d550887396f8af9afd
Igor Papandinas <igor.papandinas@posteo.net>
2024-01-08 23:08:58 +0100

    feat: Footer updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,16 +76,30 @@ export default function RootLayout({
             <main className="container mx-auto max-w-7xl py-16 px-6 flex-grow">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-                title="nextui.org homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">NextUI</p>
-              </Link>
+            <footer className="py-6 md:px-8 md:py-0">
+              <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+                <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+                  Built by{" "}
+                  <a
+                    href={SITE_CONFIG.links.twitter}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="font-medium underline underline-offset-4"
+                  >
+                    Kudos
+                  </a>
+                  . The source code is available on{" "}
+                  <a
+                    href={SITE_CONFIG.links.github}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="font-medium underline underline-offset-4"
+                  >
+                    GitHub
+                  </a>
+                  .
+                </p>
+              </div>
             </footer>
           </div>
         </Providers>
--- data/config.ts
+++ data/config.ts
@@ -3,7 +3,8 @@ export const SITE_CONFIG = {
   description:
     "A portal for the Substrate, Polkadot and Kusama ecosystem contributors. Find collaborations, collect kudos.",
   links: {
-    github: "https://github.com/kudos-ink",
+    github: "https://github.com/kudos-ink/portal",
+    twitter: "https://twitter.com/kudos_ink",
     bugReport:
       "https://github.com/kudos-ink/portal/issues/new?assignees=&labels=bug&projects=&template=bug_report.yml&title=",
   },
