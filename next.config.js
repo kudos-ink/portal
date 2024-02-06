8576e00dd93ee46a28fcc730c3969c409871f7ec
Leandro <leapalazzolo@gmail.com>
2024-02-06 12:22:24 -0300

    feat: add security headers

--- next.config.js
+++ next.config.js
@@ -8,6 +8,38 @@ const nextConfig = {
       },
     ],
   },
+  reactStrictMode: true,
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          // {
+          //   key: "Content-Security-Policy",
+          //   value:
+          //     "default-src 'self'; image-src 'self' 'https://avatars.githubusercontent.com'; script-src 'self' https://www.google-analytics.com; font-src 'self' 'https://fonts.googleapis.com'",
+          // },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(); battery=(); geolocation=(); microphone=()",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
