3e65ccc6e2a4c7d620f815a20c993fa872a12998
Leandro Palazzolo <leapalazzolo@gmail.com>
2024-10-09 09:10:36 -0300

    feat: add new link

--- data/config.ts
+++ data/config.ts
@@ -9,7 +9,7 @@ export const SITE_CONFIG = {
     bugReport:
       "https://github.com/kudos-ink/portal/issues/new?assignees=&labels=bug&projects=&template=bug_report.yml&title=",
     includeProject:
-      "https://github.com/kudos-ink/repository-classification/issues/new?labels=new-project&template=NEW_PROJECT.yaml&title=%5BAdd%5D%3A+",
+      "https://github.com/kudos-ink/project-classification/issues/new?assignees=&labels=add-project&projects=&template=add_project.yaml&title=%5BAdd+a+project%5D",
     maintainerFeedback: "https://eu.jotform.com/form/233383475336057",
     contributorFeedback: "https://eu.jotform.com/form/233386452273055",
   },
