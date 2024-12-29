3b473361c1d38e8a3838e984138916737ec1abbb
Igor Papandinas <26460174+ipapandinas@users.noreply.github.com>
2024-12-29 19:37:10 +0100

    fix build

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "@/lib/react-query-client";
