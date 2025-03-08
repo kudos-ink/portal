f48070e850718b51d7bd96bd2b643830bd30e7d1
Igor Papandinas <26460174+ipapandinas@users.noreply.github.com>
2025-03-08 13:30:57 +0100

    debug layout

--- app/explore/[slug]/page.tsx
+++ app/explore/[slug]/page.tsx
@@ -40,7 +40,7 @@ export default async function ExplorePage(props: IProps) {
   const query = filtersToTasksQuery(filters);
   const tasks = await fetchTasks(query);
 
-  console.log({ params, filterOptions: JSON.stringify(filterOptions), decodedSlug, filters: JSON.stringify(filters), query });
+  console.log("ExplorePage: " + { params, filterOptions: JSON.stringify(filterOptions), decodedSlug, filters: JSON.stringify(filters), query });
 
   return <InfiniteTable initialItems={tasks} query={query} />;
 }
